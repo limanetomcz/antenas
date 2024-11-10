bb9d78683b695a7f0a3a53fdfd5624ea3413b119
Lima Neto DELL <lima@praticasti.com.br>
2024-11-10 20:08:30 -0300

    making user controller

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('users', UserController::class);
