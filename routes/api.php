f33308cea27c1e147f858a9abc789d09589b2a1d
Lima Neto DELL <lima@praticasti.com.br>
2024-11-10 20:54:14 -0300

    creating route for logout

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('users', UserController::class);
+Route::resource('users', UserController::class)->middleware('auth:api');;
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
