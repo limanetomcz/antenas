925e6beef250cedc0c40c4f89faed1b478c0597d
Lima Neto DELL <lima@praticasti.com.br>
2024-11-10 20:42:42 -0300

    creating route for login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('users', UserController::class);
+Route::post('login', [AuthController::class, 'login']);
